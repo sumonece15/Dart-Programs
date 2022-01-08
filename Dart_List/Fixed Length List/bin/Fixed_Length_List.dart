6d0ff2e6c195fd0145d42264366a8e113c296463
Sumon Hossain <sumonece15@gmail.com>
2022-01-08 22:19:45 +0600

    Fixed Length List

---
+++ Dart_List/Fixed Length List/bin/Fixed_Length_List.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  var mylist= new List(3);
+
+  mylist[0]= "Sagor";
+  mylist[1]= "Ripon";
+  mylist[2]= "Sabir";
+
+  print(mylist);
+
+
+}
---
+++ Dart_List/Fixed Length List/test/Fixed_Length_List_test.dart
@@ -0,0 +1,8 @@
+import 'package:Fixed_Length_List/Fixed_Length_List.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
