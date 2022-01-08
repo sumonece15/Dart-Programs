0f4ca686cf185daa9847680394ee9aaf4de25440
Sumon Hossain <sumonece15@gmail.com>
2022-01-08 22:30:03 +0600

    Variable1

---
+++ Dart_Variable/Dart variable1/bin/Dart_variable1.dart
@@ -0,0 +1,5 @@
+void main() {
+  String name = "Sumon";
+
+  print(name);
+}
---
+++ Dart_Variable/Dart variable1/test/Dart_variable1_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_variable1/Dart_variable1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
