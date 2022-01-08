4e6ec95e977312101d7c819b751645adca9039af
Sumon Hossain <sumonece15@gmail.com>
2022-01-08 22:27:07 +0600

    Final Constant Keyword

---
+++ Dart_Keyword/Final Constant Keyword/bin/Final_Constant_Keyword.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  final  age = 30 ;
+
+  print(age);
+
+  const students = 40;
+
+  print(students);
+
+
+}
---
+++ Dart_Keyword/Final Constant Keyword/test/Final_Constant_Keyword_test.dart
@@ -0,0 +1,8 @@
+import 'package:Final_Constant_Keyword/Final_Constant_Keyword.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
