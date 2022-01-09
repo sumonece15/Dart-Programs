4a5acc333be8889ac2b59c3ed0a31f6c71c1107d
Sumon Hossain <sumonece15@gmail.com>
2022-01-09 10:00:28 +0600

    If Statement

---
+++ Dart_Statement/If Statement/bin/If_Statement.dart
@@ -0,0 +1,9 @@
+void main () {
+  // define a variable which hold numeric value
+  var n = 80;
+
+  // if statement check the given condition
+  if (n<100){
+    print("The number is smaller than 40");
+  };
+}
