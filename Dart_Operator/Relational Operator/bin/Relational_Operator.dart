727734953f7bdd76505e6f26495b3d50daa6bb39
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:55:06 +0600

    Relational Operator

---
+++ Dart_Operator/Relational Operator/bin/Relational_Operator.dart
@@ -0,0 +1,24 @@
+void main() {
+  var a = 30;
+  var b = 20;
+
+  print("The example of Relational Operator");
+
+  var res = a>b;
+  print("a is greater than b: "+res. toString());
+
+  var res0 = a<b;
+  print("a is less than b: "+res0. toString());
+
+  var res1 = a>=b;
+  print("a is greater than or equal to b: "+res1. toString());
+
+  var res2 = a<=b;
+  print("a is less than and equal to b: "+res2. toString());
+
+  var res3 = a!= b;
+  print("a is not equal to  b: "+res3. toString());
+
+  var res4 = a==b;
+  print("a is  equal to  b: "+res4. toString());
+}  
---
+++ Dart_Operator/Relational Operator/test/Relational_Operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:Relational_Operator/Relational_Operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
