71b5a3353c384837afa9e2caf6aa35d8763654b2
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:26:33 +0600

    Assignment Operator

---
+++ Dart_Operators/Assignment Operator/bin/Assignment_Operator.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Example of Assignment operators");
+  var n1 = 10;
+  var n2 = 5;
+
+  print("n1+n2 = ${n1+n2}");
+  print("n1-n2 = ${n1-n2}");
+  print("n1*n2 = ${n1*n2}");
+  print("n1/=n2 = ${n1/n2}");
+  print("n1%n2 = ${n1%n2}");
+}  
