8b2a180dadf3a56bd8ad95f6ab50b4f6b58c1cf8
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:57:17 +0600

    Unary Operator

---
+++ Dart_Operator/Unary Operator/bin/Unary_Operator.dart
@@ -0,0 +1,12 @@
+void main() {
+  var x = 30;
+  print(x++);                  //The postfix value
+
+  var y = 25;
+  print(++y);                 //The prefix value,
+
+  var z = 10;
+  print(--z);                  //The prefix value
+
+  var u = 12;
+  print(u--);    }           //The postfix value
---
+++ Dart_Operator/Unary Operator/test/Unary_Operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:Unary_Operator/Unary_Operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
