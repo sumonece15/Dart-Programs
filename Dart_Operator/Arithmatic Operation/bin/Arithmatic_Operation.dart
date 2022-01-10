b25f9b486447e81c941fb8143a80f673dbcda91e
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:48:56 +0600

    Arithmetic Operation

---
+++ Dart_Operator/Arithmatic Operation/bin/Arithmatic_Operation.dart
@@ -0,0 +1,27 @@
+
+
+void main(){
+
+  int a = 100;
+  int b = 300;
+  int c = 200;
+
+  String s1 = "Solution1";
+  String s2 = "\nSolution2";
+  String s3 = "\nSolution3";
+  String s4 = "\nSolution4";
+
+  print(s1);
+  print(a+b+c);
+
+  print(s2);
+  print(a-b+c);
+
+  print(s3);
+  print(a*b-c);
+
+  print(s4);
+  print(a+b/c);
+
+
+}
---
+++ Dart_Operator/Arithmatic Operation/test/Arithmatic_Operation_test.dart
@@ -0,0 +1,8 @@
+import 'package:Arithmatic_Operation/Arithmatic_Operation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
