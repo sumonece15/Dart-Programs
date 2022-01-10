4ac3f839026d1fdd4bc9f6354b7e2642662bd4b0
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:51:43 +0600

    Conditional Operator

---
+++ Dart_Operator/Conditional Operator/bin/Conditional_Operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  var x = null;
+  var y = 20;
+  var val = x ?? y;
+  print(val);
+
+
+  var a = 30;
+  var output = a > 42 ? "value greater than 10":"value lesser than equal to 30";
+  print(output);
+}  
---
+++ Dart_Operator/Conditional Operator/test/Conditional_Operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:Conditional_Operator/Conditional_Operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
