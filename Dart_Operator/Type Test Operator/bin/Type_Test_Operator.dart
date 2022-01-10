95478a8e6bb4c796b4a71f0e88534c70906fb053
Sumon Hossain <sumonece15@gmail.com>
2022-01-10 22:56:32 +0600

    Type Test Operator

---
+++ Dart_Operator/Type Test Operator/bin/Type_Test_Operator.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  var num = 10;
+  var name = "Sumon";
+  print(num is int);
+  print(name is! String );
+}  
---
+++ Dart_Operator/Type Test Operator/test/Type_Test_Operator_test.dart
@@ -0,0 +1,8 @@
+import 'package:Type_Test_Operator/Type_Test_Operator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
