25614baed834164cd8d3be2990b61a4a94df1029
Sumon Hossain <sumonece15@gmail.com>
2022-01-11 13:53:37 +0600

    If Else If Statement

---
+++ Dart_Statement/If Else If Statement/bin/If_Else_If_Statement.dart
@@ -0,0 +1,24 @@
+void main() {
+  var marks = 74;
+  if(marks > 85)
+  {
+    print("Excellent");
+  }
+  else if(marks>75)
+  {
+    print("Very Good");
+  }
+  else if(marks>65)
+  {
+    print("Good");
+  }
+  else
+  {
+    print("Average");
+  }
+
+
+
+
+
+}  
---
+++ Dart_Statement/If Else If Statement/test/If_Else_If_Statement_test.dart
@@ -0,0 +1,8 @@
+import 'package:If_Else_If_Statement/If_Else_If_Statement.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
