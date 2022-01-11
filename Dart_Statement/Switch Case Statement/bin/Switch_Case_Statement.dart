a0f29a1134eeee3629f27c3f789e03549b935e6a
Sumon Hossain <sumonece15@gmail.com>
2022-01-11 13:57:03 +0600

    Switch Case Statement

---
+++ Dart_Statement/Switch Case Statement/bin/Switch_Case_Statement.dart
@@ -0,0 +1,42 @@
+void main() {
+  //Program-1
+  int n = 3;
+  switch (n) {
+    case 1:
+      print("Value is 1");
+      break;
+    case 2:
+      print("Value is 2");
+      break;
+    case 3:
+      print("Value is 3");
+      break;
+    case 4:
+      print("Value is 4");
+      break;
+    default:
+      print("Out of range");
+      break;
+  }
+
+//Program-2
+  // declaring a interger variable
+  int Roll_num = 90014;
+
+// Evalaute the test-expression to find the match
+  switch (Roll_num) {
+    case 90009:
+      print("My name is Joseph");
+      break;
+    case 90010:
+      print("My name is Peter");
+      break;
+    case 090011:
+      print("My name is Devansh");
+      break;
+
+// default block
+    default:
+      print("Roll number is not found");
+  }
+}
---
+++ Dart_Statement/Switch Case Statement/test/Switch_Case_Statement_test.dart
@@ -0,0 +1,8 @@
+import 'package:Switch_Case_Statement/Switch_Case_Statement.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
