448bb9bb8ed17b6f9ffa92ba1ed3d412d3a940ef
Sumon Hossain <sumonece15@gmail.com>
2023-12-02 17:15:26 +0600

    Update Growable_List.dart

--- Dart_List/Growable List/bin/Growable_List.dart
+++ Dart_List/Growable List/bin/Growable_List.dart
@@ -1,71 +1,142 @@
-void main(){
+// void main(){
 
-  //Add Method
-  var mylist= new List();
-  mylist.add('Sumon');
-  mylist.add('Sabbir');
-  mylist.add('Sony');
-  mylist.add('Sagor');
-  mylist.add('Sufol');
-  mylist.add('Saikat');
+//   //Add Method
+//   var mylist= new List();
+//   mylist.add('Sumon');
+//   mylist.add('Sabbir');
+//   mylist.add('Sony');
+//   mylist.add('Sagor');
+//   mylist.add('Sufol');
+//   mylist.add('Saikat');
 
-  print(mylist);
+//   print(mylist);
 
-  //AddAll Method
-  var animallist = new List();
-  animallist.addAll(["Tiger","Lion","Cow","Cat"]);
+//   //AddAll Method
+//   var animallist = new List();
+//   animallist.addAll(["Tiger","Lion","Cow","Cat"]);
 
-  print(animallist);
+//   print(animallist);
 
-  //Insert Method
-  var eltronicslist = new List();
-  eltronicslist.insert(0, "Laptop");
-  eltronicslist.insert(1, "Computer");
-  eltronicslist.insert(2, "Mobile");
-  eltronicslist.insert(3, "Fan");
-  eltronicslist.insert(4, "Light");
+//   //Insert Method
+//   var eltronicslist = new List();
+//   eltronicslist.insert(0, "Laptop");
+//   eltronicslist.insert(1, "Computer");
+//   eltronicslist.insert(2, "Mobile");
+//   eltronicslist.insert(3, "Fan");
+//   eltronicslist.insert(4, "Light");
 
-  print(eltronicslist);
+//   print(eltronicslist);
 
-  //InsertAll Method
-  var letterlist = new List();
-  letterlist.insertAll(0, ["A","B","C"]);
+//   //InsertAll Method
+//   var letterlist = new List();
+//   letterlist.insertAll(0, ["A","B","C"]);
 
-  print(letterlist);
+//   print(letterlist);
 
-  //Remove method
-  //remove value from list element
-  var waterlist = new List();
-  waterlist.add("Fresh");
-  waterlist.add("Mum");
-  waterlist.add("shamoly");
-  waterlist.add("Revera");
-  waterlist.add("Coke");
+//   //Remove method
+//   //remove value from list element
+//   var waterlist = new List();
+//   waterlist.add("Fresh");
+//   waterlist.add("Mum");
+//   waterlist.add("shamoly");
+//   waterlist.add("Revera");
+//   waterlist.add("Coke");
 
-  waterlist.remove("Mum");
-  print(waterlist);
+//   waterlist.remove("Mum");
+//   print(waterlist);
 
-  //RemoveAtt method
-  //remove index from list element
-  var drinklist = new List();
-  drinklist.add("Fresh");
-  drinklist.add("Mum");
-  drinklist.add("shamoly");
-  drinklist.add("Revera");
-  drinklist.add("Coke");
+//   //RemoveAtt method
+//   //remove index from list element
+//   var drinklist = new List();
+//   drinklist.add("Fresh");
+//   drinklist.add("Mum");
+//   drinklist.add("shamoly");
+//   drinklist.add("Revera");
+//   drinklist.add("Coke");
 
-  drinklist.removeAt(2);
-  print(drinklist);
+//   drinklist.removeAt(2);
+//   print(drinklist);
 
-  //Replacement Method
-  var toolslist = new List();
-  toolslist.add("Chair");
-  toolslist.add("Table");
-  toolslist.add("Bed");
-  toolslist.add("Rak");
-  toolslist.add("Basket");
+//   //Replacement Method
+//   var toolslist = new List();
+//   toolslist.add("Chair");
+//   toolslist.add("Table");
+//   toolslist.add("Bed");
+//   toolslist.add("Rak");
+//   toolslist.add("Basket");
 
-  toolslist.replaceRange(3, 4, ["Bangladesh"]);
-  print(toolslist);
+//   toolslist.replaceRange(3, 4, ["Bangladesh"]);
+//   print(toolslist);
 
+// }
+
+
+void main() {
+// Add Method
+var mylist = <String>[];
+mylist.add('Sumon');
+mylist.add('Sabbir');
+mylist.add('Sony');
+mylist.add('Sagor');
+mylist.add('Sufol');
+mylist.add('Saikat');
+
+print(mylist);
+
+// AddAll Method
+var animallist = <String>[];
+animallist.addAll(["Tiger", "Lion", "Cow", "Cat"]);
+
+print(animallist);
+
+// Insert Method
+var eltronicslist = <String>[];
+eltronicslist.insert(0, "Laptop");
+eltronicslist.insert(1, "Computer");
+eltronicslist.insert(2, "Mobile");
+eltronicslist.insert(3, "Fan");
+eltronicslist.insert(4, "Light");
+
+print(eltronicslist);
+
+// InsertAll Method
+var letterlist = <String>[];
+letterlist.insertAll(0, ["A", "B", "C"]);
+
+print(letterlist);
+
+// Remove method
+// remove value from list element
+var waterlist = <String>[];
+waterlist.add("Fresh");
+waterlist.add("Mum");
+waterlist.add("shamoly");
+waterlist.add("Revera");
+waterlist.add("Coke");
+
+waterlist.remove("Mum");
+print(waterlist);
+
+// RemoveAt method
+// remove index from list element
+var drinklist = <String>[];
+drinklist.add("Fresh");
+drinklist.add("Mum");
+drinklist.add("shamoly");
+drinklist.add("Revera");
+drinklist.add("Coke");
+
+drinklist.removeAt(2);
+print(drinklist);
+
+// Replacement Method
+var toolslist = <String>[];
+toolslist.add("Chair");
+toolslist.add("Table");
+toolslist.add("Bed");
+toolslist.add("Rak");
+toolslist.add("Basket");
+
+toolslist.replaceRange(3, 4, ["Bangladesh"]);
+print(toolslist);
 }
