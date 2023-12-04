7623d9a63d8ac19584caef13c19133d19a79df6f
Sumon Hossain <sumonece15@gmail.com>
2023-12-04 11:59:03 +0600

    Update Fixed_Length_List.dart

--- Dart_List/Fixed Length List/bin/Fixed_Length_List.dart
+++ Dart_List/Fixed Length List/bin/Fixed_Length_List.dart
@@ -1,12 +1,22 @@
-void main(){
+// void main(){
 
-  var mylist= new List(3);
+//   var mylist= new List(3);
 
-  mylist[0]= "Sagor";
-  mylist[1]= "Ripon";
-  mylist[2]= "Sabir";
+//   mylist[0]= "Sagor";
+//   mylist[1]= "Ripon";
+//   mylist[2]= "Sabir";
+
+//   print(mylist);
+
+
+// }
+
+void main() {
+  var mylist = <String>[];
+
+  mylist.add("Sagor");
+  mylist.add("Ripon");
+  mylist.add("Sabir");
 
   print(mylist);
-
-
 }
